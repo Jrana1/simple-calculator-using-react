c3ca0a0569ecc82233ce9b2e89bb11467ae4157c
Jrana1 <jewelbutex12@gmail.com>
2021-10-24 09:39:01 +0200

    start

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div className="calc-container">
+            <h2>react calculator is comming soon</h2>
+        </div>
+    );
+};
+
+export default Calculator;
